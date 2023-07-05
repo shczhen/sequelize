16ffef94f1c0922c277c8b79d16e5396cd907a66
czhen <56986964+shczhen@users.noreply.github.com>
2023-07-05 12:04:08 +0800

    fix: update desc

--- test.sh
+++ test.sh
@@ -28,7 +28,7 @@ EXIT_CODE=$?
 # handle test results
 if [ ${EXIT_CODE} -ne 0 ]
 then
-	echo "ERROR: django test failed!"
+	echo "ERROR: sequelize test failed!"
 	echo "=========== ERROR EXIT [${EXIT_CODE}]: FULL tidb.log BEGIN ============"
 	cat ${SEQUELIZE6_TIDB_LOG}
 	echo "=========== ERROR EXIT [${EXIT_CODE}]: FULL tidb.log END =============="
